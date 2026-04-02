2166ad130eaf86e06b7cc9f0646281ee14191f2c
Dani <daniel.rodriguezciotti@mistral.ai>
2026-04-02 14:23:52 +0200

    fix infra - use spot to reduce costs

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -9,6 +9,18 @@ resource "aws_ecs_cluster" "main" {
   tags = var.tags
 }
 
+resource "aws_ecs_cluster_capacity_providers" "main" {
+  cluster_name = aws_ecs_cluster.main.name
+
+  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = "FARGATE_SPOT"
+  }
+}
+
 resource "aws_cloudwatch_log_group" "ecs" {
   name              = "/ecs/${var.name_prefix}"
   retention_in_days = 7
